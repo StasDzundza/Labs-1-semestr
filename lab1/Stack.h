2317f7695f56ba16e8cf6d39c5bc7eba7a99bf7c
Stas Dzundza <stas.dzundza@ukr.net>
2018-09-18 15:03:36 +0300

    deleted cpp files because i cant solve lnk 2019 error

--- lab1/Stack.h
+++ lab1/Stack.h
@@ -12,6 +12,7 @@ public:
 	virtual void push_back(T a) = 0;
 	virtual void show() = 0;
 };
+
 template<typename T>
 class Stack:public stack<T>
 {
@@ -26,3 +27,31 @@ private:
 	sqd_list<T> lst;
 	sqd_arr<T> arrr;
 };
+
+template<typename T>
+void Stack<T>::set_state(int st)
+{
+	if (st == 1)
+		mode = MODE::list;
+	else
+		mode = MODE::arr;
+}
+template<typename T>
+void Stack<T>::push_back(T a)
+{
+	if (mode == MODE::list)
+		lst.add_st(a);
+	else
+	{
+		arrr.add_st(a);
+	}
+}
+
+template<typename T>
+void Stack<T>::show()
+{
+	if (mode == MODE::list)
+		lst.show_st();
+	else
+		arrr.show_st();
+}
