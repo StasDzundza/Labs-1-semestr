201b2f92e95b7b6106bcbe403d7102094740b378
Stas Dzundza <stas.dzundza@ukr.net>
2018-09-25 23:52:07 +0300

    classes in cpp files

--- lab1/Character.h
+++ lab1/Character.h
@@ -27,12 +27,10 @@ private:
 
 };
 
-Character::Character() :name("character"),role_in_book("episodic"){}
-
 ostream&operator<<(ostream&os, const Character& p)
 {
 	os << "≤м'€ персонажу : " << p.name << endl;
-	os << "…ого роль в ц≥й книз≥ : "<< p.role_in_book << endl;
+	os << "…ого роль в ц≥й книз≥ : " << p.role_in_book << endl;
 	return os;
 }
 
@@ -50,51 +48,3 @@ istream&operator>>(istream&is, Character& p)
 	}
 	return is;
 }
-
-Character& Character::operator=(const Character& other_character)
-{
-	this->name = other_character.name;
-	this->role_in_book = other_character.role_in_book;
-	return *this;
-}
-
-bool Character::operator==(const Character& other_character)
-{
-	if (this->name == other_character.name)
-	{
-		return true;
-	}
-	else return false;
-}
-
-bool Character::operator<(const Character & other_character)
-{
-	return this->name < other_character.name;
-}
-
-bool Character::operator>(const Character & other_character)
-{
-	return this->name > other_character.name;
-}
-
-string Character::get_role()
-{
-	return role_in_book;
-}
-
-void Character::get_info()
-{
-	cout << "≤м'€ персонажу : " << name << endl;
-	cout << "…ого роль в ц≥й книз≥ : " << role_in_book << endl;
-}
-
-void Character::set_name(string name)
-{
-	this->name = name;
-}
-
-void Character::set_role(string role)
-{
-	this->role_in_book = role;
-}
-
