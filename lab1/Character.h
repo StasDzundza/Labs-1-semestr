aab941a4e6629dfbdb5bc3648b900deb2bbd72a0
Stas Dzundza <stas.dzundza@ukr.net>
2018-09-25 23:54:05 +0300

    in classes 2

--- lab1/Character.h
+++ lab1/Character.h
@@ -48,3 +48,53 @@ istream&operator>>(istream&is, Character& p)
 	}
 	return is;
 }
+Character::Character() :name("character"), role_in_book("episodic") {}
+
+
+Character& Character::operator=(const Character& other_character)
+{
+	this->name = other_character.name;
+	this->role_in_book = other_character.role_in_book;
+	return *this;
+}
+
+bool Character::operator==(const Character& other_character)
+{
+	if (this->name == other_character.name)
+	{
+		return true;
+	}
+	else return false;
+}
+
+bool Character::operator<(const Character & other_character)
+{
+	return this->name < other_character.name;
+}
+
+bool Character::operator>(const Character & other_character)
+{
+	return this->name > other_character.name;
+}
+
+string Character::get_role()
+{
+	return role_in_book;
+}
+
+void Character::get_info()
+{
+	cout << "≤м'€ персонажу : " << name << endl;
+	cout << "…ого роль в ц≥й книз≥ : " << role_in_book << endl;
+}
+
+void Character::set_name(string name)
+{
+	this->name = name;
+}
+
+void Character::set_role(string role)
+{
+	this->role_in_book = role;
+}
+
--- lab1/Serie.h
+++ lab1/Serie.h
@@ -18,3 +18,64 @@ private:
 };
 
 
+Serie& Serie:: operator=(Serie& other_serie)
+{
+	this->serie.clear();
+	for (int i = 0; i < other_serie.serie.size(); i++)
+	{
+		this->serie.push_front(other_serie.serie[i]);
+	}
+	return *this;
+}
+
+//ми не будемо ніколи використовувати,але мусимо перегрузити,щоб створити стек об'єктів цього класу
+bool Serie::operator>(const Serie & other_serie)
+{
+	return false;
+}
+
+//ми не будемо ніколи використовувати,але мусимо перегрузити,щоб створити стек об'єктів цього класу
+bool Serie::operator<(const Serie & other_serie)
+{
+	return false;
+}
+
+bool Serie::operator==(Serie other_serie)
+{
+	if (this->serie.size() != other_serie.serie.size())
+		return false;
+	if (this->serie.size() == 0 && other_serie.serie.size() == 0)
+		return true;
+	for (int i = 0; i < serie.size(); i++)
+	{
+		if (!(this->serie[i] == other_serie.serie[i]))
+		{
+			return false;
+		}
+	}
+	return true;
+}
+
+ostream & operator<<(ostream & os, Serie &SERIE)
+{
+	for (int i = 0; i < SERIE.serie.size(); i++)
+	{
+		os << SERIE.serie[i];
+	}
+	return os;
+}
+
+void Serie::add_book(Book book)
+{
+	serie.push_front(book);
+}
+
+void Serie::sort_serie()
+{
+	serie.sort();
+}
+
+void Serie::show_serie()
+{
+	serie.show();
+}
