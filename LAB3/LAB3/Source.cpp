24613a03079a5db1f5cd59432f4829bf2407b60c
stas_dzundza <stas.dzundza@gmail.com>
2018-11-27 17:04:35 +0200

    add function which show trajectory of the tracking object

--- LAB3/LAB3/Source.cpp
+++ LAB3/LAB3/Source.cpp
@@ -6,6 +6,6 @@ int main()
 	VideoEditor myVideo;
 	PhotoEditor myPhoto;
 	//myVideo.track_different_colors_in_hsv();
-	myVideo.camera_with_different_effects();
+	myVideo.track_different_colors_and_show_trajectory();
 	return 0;
 }
--- LAB3/LAB3/VideoEditor.cpp
+++ LAB3/LAB3/VideoEditor.cpp
@@ -89,6 +89,112 @@ void VideoEditor::track_different_colors_in_hsv()
 
 void VideoEditor::track_different_colors_and_show_trajectory()
 {
+	VideoCapture cap(0); //capture the video from webcam
+
+	if (!cap.isOpened())  // if not success, exit program
+	{
+		cout << "Cannot open the web cam" << endl;
+		throw std::exception("Cannot open the web cam");
+	}
+
+	namedWindow("Control", CV_WINDOW_AUTOSIZE); //create a window called "Control"
+
+	int iLowH = 170;
+	int iHighH = 179;
+
+	int iLowS = 150;
+	int iHighS = 255;
+
+	int iLowV = 60;
+	int iHighV = 255;
+
+	//Create trackbars in "Control" window
+	createTrackbar("LowH", "Control", &iLowH, 179); //Hue (0 - 179)
+	createTrackbar("HighH", "Control", &iHighH, 179);
+
+	createTrackbar("LowS", "Control", &iLowS, 255); //Saturation (0 - 255)
+	createTrackbar("HighS", "Control", &iHighS, 255);
+
+	createTrackbar("LowV", "Control", &iLowV, 255);//Value (0 - 255)
+	createTrackbar("HighV", "Control", &iHighV, 255);
+
+	//last center coordinates of our traking object
+	int LastX = -1;
+	int LastY = -1;
+
+	//Capture a temporary image from the camera
+	Mat TmpImg;
+	cap.read(TmpImg);
+
+	//Create a black image with the size as the camera output
+	Mat imgLines = Mat::zeros(TmpImg.size(), CV_8UC3);;
+
+
+	while (true)
+	{
+		Mat OriginalImg;
+
+		bool bSuccess = cap.read(OriginalImg); // read a new frame from video
+
+
+
+		if (!bSuccess) //if not success, break loop
+		{
+			cout << "Cannot read a frame from video stream" << endl;
+			break;
+		}
+
+		Mat imgHSV;
+
+		cvtColor(OriginalImg, imgHSV, COLOR_BGR2HSV); //Convert the captured frame from BGR to HSV
+
+		Mat ThresholdedImg;
+
+		inRange(imgHSV, Scalar(iLowH, iLowS, iLowV), Scalar(iHighH, iHighS, iHighV), ThresholdedImg); //Threshold the image
+
+	   //morphological opening (removes small objects from the foreground)
+		erode(ThresholdedImg, ThresholdedImg, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
+		dilate(ThresholdedImg, ThresholdedImg, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
+
+		//morphological closing (removes small holes from the foreground)
+		dilate(ThresholdedImg, ThresholdedImg, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
+		erode(ThresholdedImg, ThresholdedImg, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
+
+		//Calculate the moments of the thresholded image
+		Moments oMoments = moments(ThresholdedImg);
+
+		double dM01 = oMoments.m01;
+		double dM10 = oMoments.m10;
+		double dArea = oMoments.m00;
+
+		// if the area <= 10000, I consider that the there are no object in the image and it's because of the noise, the area is not zero 
+		if (dArea > 10000)
+		{
+			//calculate the position of the ball
+			int posX = dM10 / dArea;
+			int posY = dM01 / dArea;
+
+			if (LastX >= 0 && LastY >= 0 && posX >= 0 && posY >= 0)
+			{
+				//Draw a red line from the previous point to the current point
+				line(imgLines, Point(posX, posY), Point(LastX, LastY), Scalar(0, 0, 255), 2);
+			}
+
+			LastX = posX;
+			LastY = posY;
+		}
+
+		imshow("Thresholded Image", ThresholdedImg); //show the thresholded image
+
+		OriginalImg = OriginalImg + imgLines;
+		imshow("Original", OriginalImg); //show the original image
+
+		if (waitKey(30) == 27) //wait for 'esc' key press for 30ms. If 'esc' key is pressed, break loop
+		{
+			cout << "esc key is pressed by user" << endl;
+			break;
+		}
+	}
 }
 
 void VideoEditor::track_red_color_objects()
